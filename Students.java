3393304359925de904c2359efec47211c40573a8
Kismat-adhikari <kismatadhikari62@gmail.com>
2025-05-31 11:46:50 +0545

    Adding all the java files

---
+++ Students.java
@@ -0,0 +1,31 @@
+public class Students {
+    private String name;
+    private String id;
+    private final double gpa;
+
+    public Students(String name, String id, double gpa) {
+        this.name = name;
+        this.id = id;
+        this.gpa = gpa;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+}
